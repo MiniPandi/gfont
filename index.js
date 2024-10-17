b9d83ec586c564c5f07fec6802ec82caeb3310eb
MiniPandi <admin@minipandi.net>
2024-10-17 21:59:32 +0200

    dies das annanas

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ const cors = require('cors');
 const app = express();
 const PORT = 5409;
 
+app.use(cors())
 app.use('/css', express.static('css'));
 app.use('/fonts', express.static('fonts'));
-app.use(cors())
+
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
