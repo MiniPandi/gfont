cdad57a8f707dfa809c8eb20db85ab672f3c4a4f
MiniPandi <admin@minipandi.net>
2024-10-17 21:45:56 +0200

    asdasd

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 
 const app = express();
-const PORT = 5409
+const PORT = 5409;
 
-app.use(express.static('fonts'));
-app.use(express.static('css'));
+app.use('/css', express.static('css'));
+app.use('/fonts', express.static('fonts'));
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
-})
+});
