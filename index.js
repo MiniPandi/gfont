53b9ac803d6b6092003d8c63aac939963f06a1a1
MiniPandi <admin@minipandi.net>
2024-10-17 21:55:24 +0200

    i hate cors

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
 const PORT = 5409;
 
 app.use('/css', express.static('css'));
 app.use('/fonts', express.static('fonts'));
+app.use(cors())
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
