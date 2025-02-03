d3fe0b0a17867709f8012be08ff1c1d7c00f3e5f
MiniPandi <me@minipandi.net>
2025-02-03 10:57:52 +0100

    added indexer

--- index.js
+++ index.js
@@ -5,6 +5,29 @@ const app = express();
 const PORT = 5409;
 
 app.use(cors())
+app.use((req, res, next) => {
+    const filePath = path.join(process.env.NAME, req.path);
+    if (!fs.existsSync(filePath)) {
+        return res.status(404).send("File not found");
+    }
+    const stats = fs.statSync(filePath);
+    if (stats.isDirectory()) {
+        let files = fs.readdirSync(filePath);
+        if (files.length === 0) {
+            return res.status(404).send("File not found");
+        }
+        let returnStr = '';
+        for (let i = 0; i < files.length; i++) {
+            let fullLink = path.join(req.path, files[i]);
+            returnStr += `<a href="${fullLink}">${files[i]}</a><br>`;
+        }
+        return res.send(`<a href="../">../</a><br>` + returnStr);
+    }
+    if (req.query.download === 'true') {
+        res.setHeader('Content-Disposition', 'attachment');
+    }
+    next();
+});
 app.use('/fonts', express.static('fonts'));
 
 
